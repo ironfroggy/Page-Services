574ff3223696431c6184e152d9a906ef21a379a8
Calvin Spealman <ironfroggy@gmail.com>
2011-01-10 21:03:49 -0500

    Implementation and test of responses to requests.

--- pageservices/injection.js
+++ pageservices/injection.js
@@ -12,12 +12,35 @@ PSE.ping = function() {
 };
 
 window.addEventListener('pseRequest', function(ev) {
-    console.log(JSON.parse($(document.body).attr('data-__PSE_DATA')));
+    var pse_request_params = JSON.parse($(document.body).attr('data-__PSE_DATA'));
+    var data = pse_request_params.data;
+    var callback_id = pse_request_params.callback_id;
     PSE.ping();
+
+    $(document.body).attr('data-__PSE_CALLBACK_ID', callback_id);
+    $(document.body).attr('data-__PSE_DATA', JSON.stringify({status: "pong"}));
+    var ev = document.createEvent("Events");
+    ev.initEvent("pseResponse", false, true);
+    window.dispatchEvent(ev);
 }, false, true);
 
 $(document.body).append('<script type="text/javascript">'+
 'var PSE={'+
-'_sendRequest:function(data){document.body.setAttribute("data-__PSE_DATA", JSON.stringify(data));var ev=document.createEvent("Events");ev.initEvent("pseRequest", false, true);window.dispatchEvent(ev);}'+
+'_requestCallbacks: {},'+
+'_nextCallbackID: 0,'+
+'_sendRequest: function(data, callback) {'+
+'   document.body.setAttribute("data-__PSE_DATA", JSON.stringify({data:data, callback_id:PSE._nextCallbackID}));'+
+'   PSE._requestCallbacks[PSE._nextCallbackID] = callback;'+
+'   PSE._nextCallbackID = PSE._nextCallbackID + 1;'+
+'   var ev=document.createEvent("Events");'+
+'   ev.initEvent("pseRequest", false, true);'+
+'   window.dispatchEvent(ev);'+
+'}'+
 '};'+
+'window.addEventListener("pseResponse", function(ev) {'+
+'   var response = JSON.parse(document.body.getAttribute("data-__PSE_DATA"));'+
+'   var callback_id = document.body.getAttribute("data-__PSE_CALLBACK_ID");'+
+'   PSE._requestCallbacks[callback_id](response);'+
+'   delete PSE._requestCallbacks[callback_id];'+
+'});'+
 '</script>');
