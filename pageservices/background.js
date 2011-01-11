acc99813e3605d9dab1497b207f568ab130ac7f1
Calvin Spealman <ironfroggy@gmail.com>
2011-01-10 23:57:00 -0500

    Add the first service, a locally hosted echo service.

--- pageservices/background.js
+++ pageservices/background.js
@@ -23,8 +23,6 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 var count = 0;
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
-        console.log("ping received");
-
         if (request.data.do) {
             pse_methods[request.data.do](request, sendResponse);
         }
@@ -49,6 +47,7 @@ var pse_methods = {
         sendResponse({status: "registered"});
     },
     requestService: function(request, sendResponse) {
+        addServiceIFrame(request.data.service);
         sendResponse({status: "found"});
     },
     callService: function(request, sendResponse) {
@@ -58,3 +57,41 @@ var pse_methods = {
     }
 };
 
+var service_urls = {
+    "echo": "/echo.html"
+};
+function addServiceIFrame(service) {
+    var iframe = getServiceIFrame(service);
+    if (iframe === undefined) {
+        $(document.body).append('<iframe src="' + service_urls[service] + '" id="service_frame-' + service + '" />');
+        //window.setTimeout(0, function(){
+        //    iframe = getServiceIFrame(service);
+        //    iframe.contentWindow.postMessage({do: "pse_init"}, "*");
+        //});
+    }
+};
+function getServiceIFrame(service) {
+    return $('#service_frame-'+service)[0];
+};
+
+var pse_service_callback_id = 0;
+var pse_service_callbacks = {};
+window.addEventListener("message", function(ev) {
+    var data = JSON.parse(ev.data);
+    console.log(data);
+    if (data.do === "provideService") {
+        if (typeof services[data.service] === "undefined") {
+            services[data.service] = {};
+        }
+        services[data.service][data.method] = function(params, cb) {
+            getServiceIFrame(data.service).contentWindow.postMessage(JSON.stringify({do: "pse_call", method: data.method, params: params, callback_id: pse_service_callback_id}), "*");
+            pse_service_callbacks[pse_service_callback_id] = function(params) {
+                cb(params);
+            }
+            pse_service_callback_id = pse_service_callback_id + 1;
+        }
+    } else if (data.do === "return") {
+        pse_service_callbacks[data.callback_id](data.params);
+        delete pse_service_callbacks[data.callback_id];
+    }
+}, false);
